2fb7cd888187b0e8cb13f16e872c44784a37bca8
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-27 15:21:17 -0300

    Feat: Criei função para pegar os dados da API

---
+++ public/scripts/index.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('form')
+const containerPosts = document.querySelector('#posts')
+
+document.addEventListener('DOMContentLoaded', () => {
+  getPosts();
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+})
+
+const getPosts = async () => {
+  const JSONOfPosts = await fetch('http://127.0.0.1:3000/post/all')
+    .then(res => res.json());
+
+  const posts = JSON.parse(JSONOfPosts);
+
+  posts.forEach(post => {
+    containerPosts.innerHTML += `
+      <div id="${post.id}" class="card-post">
+        <div class="content-card-post-top">
+          <h3>${post.title}</h3>
+        </div>
+        <div class="content-card-post-bottom">
+          <p>${post.description}</p>
+        </div>
+      </div>
+    `
+  });
+}
